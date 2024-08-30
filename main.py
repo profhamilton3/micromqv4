1fcdf374f22d08df4b51bef24b4966e583e28267
T. Hamilton <profhamilton3@gmail.com>
2024-08-30 12:19:38 -0700

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 
 def onIn_background():
     while True:
-        
-        if maqueen.ultrasonic(10):
-            pass
+        if 10 > maqueen.ultrasonic(PingUnit.CENTIMETERS):
+            music.play(music.string_playable("C5 B A G F E D C ", 300),
+            music.PlaybackMode.UNTIL_DONE)
         else:
-            pass
-        
+            basic.pause(100)   
+
 control.in_background(onIn_background)
+
+def do_drive_forwad(speed:number, duration: number):
+    maqueen.write_led(maqueen.LED.LED_ALL, maqueen.LEDswitch.TURN_ON)
+    maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, speed )
+    basic.pause(duration)
+    maqueen.motor_stop(maqueen.Motors.ALL)
+
+
+
+
+def on_logo_event_pressed():
+    music.built_in_playable_melody(Melodies.POWER_UP)
+    do_drive_forwad(100,1000)
+
+
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
+
+base.show_icon(IconNames.CONFUSED)
--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 control.inBackground(function onIn_background() {
     while (true) {
-        if (maqueen.Ultrasonic(10)) {
-            
+        if (10 > maqueen.Ultrasonic(PingUnit.Centimeters)) {
+            music.play(music.stringPlayable("C5 B A G F E D C ", 300), music.PlaybackMode.UntilDone)
         } else {
-            
+            basic.pause(100)
         }
         
     }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    music.builtInPlayableMelody(Melodies.PowerUp)
+    do_drive_forwad
 })
